c2c54fe337e9d4616e7cb3d2e5e469ec2661d0c0
yuuura87 <yuuura87@gmail.com>
2017-08-26 13:03:21 +0300

    Minor change

--- app/src/main/java/com/example/yuuura87/minesweeper/LevelActivity.java
+++ app/src/main/java/com/example/yuuura87/minesweeper/LevelActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.Button;
-
 import com.google.gson.Gson;
 
 public class LevelActivity extends AppCompatActivity {
